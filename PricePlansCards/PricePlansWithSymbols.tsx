75e67a2d85a0c3948447c7e33f89791885143628
csathishkumar <csathishkumar2002@gmail.com>
2024-11-29 13:25:34 +0530

    Price Cards with specific Price Symbols

---
+++ PricePlansCards/PricePlansWithSymbols.tsx
@@ -0,0 +1,89 @@
+/*
+BASIC PLAN              PREMIUM PLAN                ENTERPRISE PLAN
+    99                       999                          9999
+*/
+
+import type { ComponentType } from "react"
+import { createStore } from "https://framer.com/m/framer/store.js@^1.0.0"
+
+const useStore = createStore({
+    period: "month",
+    symbol: "$",
+    price: {
+        basic: ["29", "222"],
+        premium: ["59", "555"],
+        enterprise: ["99", "999"],
+    },
+    countrySymbol: {
+        dollar: ["$", "₹"],
+    },
+})
+
+export function withTogglePeriod(Component): ComponentType {
+    return (props) => {
+        const [store, setStore] = useStore()
+        const newStore = store.period === "month" ? "year" : "month"
+        const variant = store.period === "month" ? "Monthly" : "Annual"
+        // console.log(store.period)
+        return (
+            <Component
+                {...props}
+                variant={variant}
+                onClick={() => setStore({ period: newStore })}
+            />
+        )
+    }
+}
+
+export function SymbolFun(Component): ComponentType {
+    return (props) => {
+        const [store, setStore] = useStore()
+        const newStore = store.symbol === "$" ? "₹" : "$"
+        const variant = store.symbol === "$" ? "USD" : "INR"
+        // console.log(store.period)
+        return (
+            <Component
+                {...props}
+                variant={variant}
+                onClick={() => setStore({ symbol: newStore })}
+            />
+        )
+    }
+}
+
+export function SymbolBro(Component): ComponentType {
+    return (props) => {
+        const [store, setStore] = useStore()
+        const index = store.symbol === "$" ? 0 : 1
+        return <Component {...props} text={store.countrySymbol.dollar[index]} />
+    }
+}
+
+export function withBasicPrice(Component): ComponentType {
+    return (props) => {
+        const [store, setStore] = useStore()
+        const index = store.period === "month" ? 0 : 1
+        return <Component {...props} text={store.price.basic[index]} />
+    }
+}
+export function withPremiumPrice(Component): ComponentType {
+    return (props) => {
+        const [store, setStore] = useStore()
+        const index = store.period === "month" ? 0 : 1
+        return <Component {...props} text={store.price.premium[index]} />
+    }
+}
+export function withEnterprisePrice(Component): ComponentType {
+    return (props) => {
+        const [store, setStore] = useStore()
+        const index = store.period === "month" ? 0 : 1
+        return <Component {...props} text={store.price.enterprise[index]} />
+    }
+}
+
+export function withPeriod(Component): ComponentType {
+    return (props) => {
+        const [store, setStore] = useStore()
+        return <Component {...props} text={`${store.period}`} />
+    }
+}
